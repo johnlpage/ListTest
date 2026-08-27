6e62baa06b0fe124c2b8f77d0b3e9b6c54c99a8b
johnlpage <johnlpage@gmail.com>
2026-08-27 17:28:45 +0100

    Double threads

--- memex/src/main/java/com/johnlpage/memex/config/AsyncLoadConfig.java
+++ memex/src/main/java/com/johnlpage/memex/config/AsyncLoadConfig.java
@@ -13,8 +13,8 @@ public class AsyncLoadConfig {
   @Bean(name = "loadExecutor")
   public ThreadPoolTaskExecutor loadExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(16);
-    executor.setMaxPoolSize(16);
+    executor.setCorePoolSize(32);
+    executor.setMaxPoolSize(32);
     executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
     executor.setQueueCapacity(0);
     executor.setThreadNamePrefix("AsyncLoadThread-");
