616bb69a6ae88e25b787e730585948f54b94ec91
johnlpage <johnlpage@gmail.com>
2026-09-01 15:07:43 +0100

    Fixed to put current date not fixed string when futzing

--- memex/src/main/java/com/johnlpage/memex/Listing/service/ListingPreWriteTriggerService.java
+++ memex/src/main/java/com/johnlpage/memex/Listing/service/ListingPreWriteTriggerService.java
@@ -3,28 +3,33 @@ package com.johnlpage.memex.Listing.service;
 import com.johnlpage.memex.Listing.model.Listing;
 import com.johnlpage.memex.Listing.repository.ListingRepository;
 import com.johnlpage.memex.generics.service.PreWriteTriggerService;
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ListingPreWriteTriggerService
-    extends PreWriteTriggerService<Listing> {
+        extends PreWriteTriggerService<Listing> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ListingPreWriteTriggerService.class);
+    private final AtomicBoolean hasLoggedFirstCall = new AtomicBoolean(false);
 
     public ListingPreWriteTriggerService(ListingRepository repository) {
         // Initialize any required dependencies
     }
 
     /*
-     * This code will be very specific to your data and how you want to test it.
+     * This code will be very specific to your data and
+     * how you want to test it.
      * May not be required.
      */
 
     /** This Code is for mutable models */
     @Override
     public void modifyMutableDataPreWrite(Listing document) {
-    // Controller is set up to only inject this if futz=true
-
-            document.setDescription(document.getDescription() + " (updated)");
-
+        document.setDescription(document.getDescription() + Instant.now().toString());
     }
 
     /* This Code is used for immutable models */
